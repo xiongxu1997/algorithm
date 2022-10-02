df2c1bd7991d2ba5898ab3459911034579d3c947
xiongxu1997 <1005316198@qq.com>
2022-10-02 18:52:29 +0800

    add binary search

---
+++ binary_search.py
@@ -0,0 +1,21 @@
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+# 二分查找
+def search(a, target):
+    n = len(a)
+    low = 0
+    high = n - 1
+    while low <= high:
+        mid = low + ((high - low) >> 1)
+        if a[mid] > target:
+            high = mid - 1
+        elif a[mid] < target:
+            low = mid + 1
+        else:
+            return mid
+    return -1
+
+
+if __name__ == '__main__':
+    print(search(a, 5))
